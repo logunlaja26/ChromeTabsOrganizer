8f1f1c948a275b1fac023813eda72335c80ee952
logunlaja26 <logunlaja26@gmail.com>
2021-11-23 13:59:41 -0600

    Added function structure to group tabs

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 let displayBtn = document.getElementById("input-btn");
+let groupBtn = document.getElementById("group-btn");
 
 displayBtn.addEventListener("click", function () {
-  console.log("this is my project......");
+  console.log("******The Beginning of the Project********");
   getPropertiesOfAllTabs();
+});
+
+groupBtn.addEventListener("click", function () {
+  console.log("******This is for the grouping functionality******");
+  groupTabs();
 });
 
 function getPropertiesOfAllTabs() {
   chrome.tabs.query({}, function (tabs) {
     tabs.forEach((tab) => {
-      //console.log(`Various urls... , ${tab.url}`);
+      console.log(tab.id);
       console.log(tab.url);
     });
+  });
+}
+function groupTabs() {
+  chrome.tabs.group({}, function () {
+    console.log("grouping..");
   });
 }
